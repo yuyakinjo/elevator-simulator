09f421779b33c7839a7dec99d9aaa2fd1224ef2e
kinjo yuya <kinjo.bobchin@gmail.com>
2022-02-04 20:10:28 +0900

    chore: 基本的な動き追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import anime, { AnimeInstance } from 'animejs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'elevator-simulator';
+export class AppComponent implements OnInit {
+  anime!: AnimeInstance;
+  ngOnInit(): void {
+    const targets = document.querySelector('.room');
+    this.anime = anime({ targets, translateY: '-100', autoplay: false });
+  }
+
+  start() {
+    this.anime.play();
+  }
+
+  pause(): void {
+    this.anime.pause();
+  }
+
+  restart(): void {
+    this.anime.restart();
+  }
+
+  reverse(): void {
+    this.anime.reverse();
+  }
 }
