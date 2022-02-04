5d8d19ccce0dba7663469440a74994063e75e5d2
kinjo yuya <kinjo.bobchin@gmail.com>
2022-02-04 20:10:15 +0900

    feat: icon追加

--- src/app/modules/materials.module.ts
+++ src/app/modules/materials.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  exports: [MatButtonModule],
+  exports: [MatButtonModule, MatIconModule],
 })
 export class MaterialsModule {}
